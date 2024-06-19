d572e042dc37aab6725f39ecdd534264f8c322ba
Evans Nawu <evans.nawu@wconsulting.mu>
2024-06-19 16:07:33 +0400

    feat: add filter and model relationship

---
+++ app/Models/Task.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use App\Observers\TaskObserver;
+use App\Traits\FilterByOwner;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+
+#[ObservedBy([TaskObserver::class])]
+class Task extends Model
+{
+    use HasFactory,FilterByOwner;
+
+    protected $fillable = [
+        'title',
+        'duedate',
+        'status',
+        'description',
+        'user_id'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
